9e9f3b9dda99db4218da7cb320a6709d52ec031d
JakeSoffer <jake@rollioforce.com>
2018-07-03 15:30:01 +0000

    First Commit.

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+//
+//  hello.cpp
+//  
+//
+//  Created by Jake Soffer on 7/3/18.
+//
+
+#include <stdio.h>
+#include "std_lib_facilities.h"
+
+int main()
+{
+    cout << "Hello World!\n";
+    keep_window_open();
+    return 0;
+}
